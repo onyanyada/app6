4a1367bdc056834208c71b314e202a6d040c2b20
uetani <shiiizuka.furuta@gmail.com>
2024-10-16 13:55:53 +0900

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator; // Validatorのインポート
+use Illuminate\Support\Facades\Auth; // Authのインポート
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $posts = Post::where('user_id', Auth::user()->id)->orderBy('created_at', 'asc')->get();
+        return view('posts', [
+            'posts' => $posts
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //バリデーション
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|min:1|max:255',
+            'body' => 'required | min:1 | max:255',
+            'is_public' => 'required | max:6'
+        ]);
+
+        //バリデーション:エラー 
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+        //以下に登録処理を記述（Eloquentモデル）
+
+        // Eloquentモデル
+        $posts = new Post;
+        $posts->user_id  = Auth::user()->id;
+        $posts->title   = $request->title;
+        $posts->body = $request->body;
+        $posts->is_public = $request->is_public;
+        $posts->save();
+        return redirect('/');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit($post_id)
+    {
+        $posts = Post::where('user_id', Auth::user()->id)->find($post_id);
+        return view('postsedit', ['post' => $posts]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Post $post)
+    {
+        //バリデーション
+        $validator = Validator::make($request->all(), [
+            'id' => 'required',
+            'title' => 'required|min:1|max:255',
+            'body' => 'required | min:1 | max:255',
+            'is_public' => 'required | max:6'
+        ]);
+        //バリデーション:エラー
+        if ($validator->fails()) {
+            return redirect('/postsedit/' . $request->id)
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        //データ更新
+        $posts = Post::find($request->id);
+        $posts = Post::where('user_id', Auth::user()->id)->find($request->id);
+        $posts->title   = $request->title;
+        $posts->body = $request->body;
+        $posts->is_public = $request->is_public;
+        $posts->save();
+        return redirect('/');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();       //追加
+        return redirect('/');  //追加
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Http\Controllers\PostController; //Add
+use App\Models\Post; //Add
+
+Route::group(
+    ['middleware' => 'auth'],
+    function () {
+        //本：ダッシュボード表示(books.blade.php)
+        Route::get('/', [PostController::class, 'index'])->name('post_index');
+        Route::get('/dashboard', [PostController::class, 'index'])->name('dashboard');
+
+        //本：追加 
+        Route::post('/posts', [PostController::class, "store"])->name('post_store');
+
+        //本：削除 
+        Route::delete('/post/{post}', [PostController::class, "destroy"])->name('post_destroy');
+
+        //本：更新画面
+        Route::post('/postsedit/{post}', [PostController::class, "edit"])->name('post_edit'); //通常
+        Route::get('/postsedit/{post}', [PostController::class, "edit"])->name('edit');      //Validationエラーありの場合
+
+        //本：更新画面
+        Route::post('/posts/update', [PostController::class, "update"])->name('post_update');
+    }
+);
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
