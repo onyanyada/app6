3b8a65dffb7f7a0c4dc0df03da6de576f26a7e8e
uetani <shiiizuka.furuta@gmail.com>
2024-10-19 22:40:12 +0900

    ホームのカラム表示変更

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -35,7 +35,7 @@
        
     
     <!--右側エリア[START]-->
-    <div class="grid grid-cols-2 gap-4 w-screen text-gray-700 text-left px-4 py-2 m-2">
+    <div class="grid grid-cols-2 gap-4 text-gray-700 text-left px-4 py-2 m-2">
          <!-- 現在の本 -->
         @if (count($posts) > 0)
             @foreach ($posts as $post)
